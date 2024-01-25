487affa157b3a52d652b04a7c44ce85ae4565a7a
deadfry42 <75206733+memyboi@users.noreply.github.com>
2024-01-25 20:50:10 +0000

    Update webptopng.sh

--- Reverts/Images/webptopng.sh
+++ Reverts/Images/webptopng.sh
@@ -1,1 +1,5 @@
 for f in ./*.webp; do dwebp "$f" -o "$(basename $f .webp).png"; rm -rf "$f"; done
+
+# This is a simple bash tool to convert .webp files (as found on the terraria.wiki.gg/ site) into .png images w/ dwebp.
+# will likely not work on windows and/or mac, don't confuse urself w/ this.
+# probably gonna just mass import sprites ngl
